63fff6e0e92522e7764f29ca5bc24f5870470664
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2017-09-08 18:13:49 +0200

    More work

---
+++ templates/reserves-espai.tpl.php
@@ -0,0 +1,6 @@
+<?php $content = $element->content; ?>
+<?php dpm($content); ?>
+
+<?php print render($content['reserves_espai_body']); ?>
+
+<?php print render($content['reserves_espai_times']); ?>
